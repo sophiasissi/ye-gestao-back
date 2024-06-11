6516466013761a5ca14c766a88e2b077fd2e60ce
sophiasissi <sophiacurcioguedes@gmail.com>
2024-06-11 19:40:35 -0300

    Updating

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+
+const port = process.env.DB_PORT as number | undefined
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    url: process.env.DB_URL,
+    ssl:{
+        rejectUnauthorized: false,
+    },
+    entities: [`${__dirname}/**/entities/*.{ts,js}`],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
+})
